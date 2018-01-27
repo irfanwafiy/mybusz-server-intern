73b10828c36b7f4e41ab0858be84ae8a02e9f904
unknown <skyxsw7@gmail.com>
2018-01-27 23:44:59 +0800

    revert url move to web.php

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,29 @@ Route::get('/getETA', 'getBusInfoController@getETA');
 Route::get('/updateLocation', 'getBusInfoController@updateLocation');
 
 
+Route::post('/Ian_updateLocation','userController@Ian_updateLocation');
+Route::post('/calculateETATest','userController@calculateETATest');
+Route::post('/calculateETA','userController@calculateETA');
+Route::post('/viewETATable','userController@viewETATable');
+Route::post('/convertBustoptoNearestPolyLine','userController@convertBustoptoNearestPolyLine');
+Route::post('/getData','userController@getData');
+Route::post('/getKM','userController@getKM');
+Route::post('/testgetKM','userController@testgetKM');
+Route::post('/testCal','userController@testCal');
+Route::post('/getBusRoute','userController@getBusRoute');
+Route::post('/checkClosePointExist','userController@checkClosePointExist');
+Route::post('/calculateETAWin','userController@calculateETAWin');
+Route::post('/calculateHistoricDataAverage','userController@calculateHistoricDataAverage');
+
+
+Route::post('/calculateHistoricData','userController@calculateHistoricData');
+Route::post('/ianTest','userController@ianTest');
+Route::post('/getETA_schedule','userController@getETA_schedule');
+Route::post('/simulator_insertlocation','userController@simulator_insertlocation');
+
+
+Route::post('/pushCurrentData','userController@pushCurrentData');
+Route::post('/getETA','userController@getETA');
+Route::post('/getBusStopServices','userController@getBusStopServices');
 
 
