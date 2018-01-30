c083c34ef424494c2056dee9716e0f1e36a3f4d3
unknown <skyxsw7@gmail.com>
2018-01-31 03:59:47 +0800

    fix response

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -471,8 +471,8 @@ class DatabaseController
 										->limit(1)
 										->get();
 		
-		print($getBusIDByBeacon_Query->'bus_id');
-		return $getBusIDByBeacon_Query->'bus_id';
+		print($getBusIDByBeacon_Query['bus_id']);
+		return $getBusIDByBeacon_Query;
 	}
 	
 	public function getBusServiceNo($route_id,$bus_id)
