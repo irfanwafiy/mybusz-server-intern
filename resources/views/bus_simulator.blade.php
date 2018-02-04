1205c1e33db45ebb26dd0f85df73ef26afdbf934
unknown <skyxsw7@gmail.com>
2018-02-04 19:06:38 +0800

    modify bus simulator view

--- resources/views/bus_simulator.blade.php
+++ resources/views/bus_simulator.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Bus Simulator</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -83,6 +83,11 @@ input[type=submit] {
     border-radius: 4px;
     cursor: pointer;
 }
+form, label {
+	color: Black;
+	font-size: 20px;
+	font-weight: 1000;
+}
 
 input[type=submit]:hover {
     background-color: #45a049;
@@ -98,56 +103,40 @@ input[type=submit]:hover {
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+        
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                
 				 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-				<br><br>
+                
 				<div class="myForm">
+				<div class="title m-b-md">
+                    Bus Simulator
+                </div>
 				<form action="https://laravelsyd-fypfinalver.herokuapp.com/bus_insertlocation" method="post">
   
-				  <label for="bus_id">bus_id:</label>
+				  <label for="bus_id">Bus_id:</label>
 				  <input id="bus_id" type="text" name="bus_id" value="">
 				  <br><br>
-				  <label for="route_id">route_id:</label>
+				  <label for="route_id">Route_id:</label>
 				  <input id="route_id" type="text" name="route_id" value="">
 				  <br><br>
-				  <label for="imei">imei:</label>
+				  <label for="imei">Imei:</label>
 				  <input id="imei" type="text" name="imei" value="">
 				  <br><br>
-				  <label for="latlong">latlong:</label>
+				  <label for="latlong">LatLong:</label>
 				  <input id="latlong" type="text" name="latlong" value="">
 				  <br><br>
-				  <label for="speed">speed:</label>
+				  <label for="speed">Speed:</label>
 				  <input id="speed" type="text" name="speed" value="">
 				  <br><br>
-				  <label for="date">date:</label>
+				  <label for="date">Date:</label>
 				  <input id="date" type="text" name="date" value="">
 				  <br><br>
 				  <input type="submit" value="Submit">
 				</form>
 				</div> 
             </div>
-        </div>
+			
     </body>
 </html>
