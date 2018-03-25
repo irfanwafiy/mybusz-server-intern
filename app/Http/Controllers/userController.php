25b5f0293b20aeffd354af4d7596b5d92ce15134
unknown <skyxsw7@gmail.com>
2018-03-25 19:15:51 +0800

    fix syntax

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -1981,28 +1981,20 @@ class userController extends Controller
                                        
                                        for($o=0; $o<sizeof($data); $o++)
                                        {
-												$compare1 = "".$data[$o]->start;
-												$compare2 = "".$busstop[$i];
-												print($compare2."===========".$compare1);
-												print("<br>");
-												print(strcmp($compare2,$compare1));
 												
-                                                 if(strcmp($compare1,$compare2) === 0)
+                                                 if($data[$o]->start == $busstop[$i])
                                                  {
 													 
-													 print($data[$o]->start);
-												
+													 
                                                           $avgSpeed = $avgSpeed + $data[$o]->avg_speed;
                                                           $avgTime = $avgTime + $data[$o]->avg_time;
                                                           $counter = $counter + 1;
                                                           $getDay = $data[$o]->day;
                                                           $busstopid_previous = $data[$o]->bus_stop_id_previous;
                                                           $busstopid_next = $data[$o]->bus_stop_id_next;
-														  print($busstop[$i]);
                                                  }
                                        }
                                        
-									   
                                        $avgSpeedFinal = $avgSpeed / $counter;
                                        $avgTimeFinal = $avgTime / $counter;
                                        
