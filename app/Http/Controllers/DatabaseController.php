37ceef00b0781f49ebb88240753a9d46d86ebccb
unknown <skyxsw7@gmail.com>
2018-04-02 20:34:45 +0800

    enhancement

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -587,7 +587,7 @@ class DatabaseController
 	{
 		$getAllBusIDByBeacon_Query = DB::table('bus')
 									->select('bus.bus_id','bus.beacon_mac')
-									->from('bus_route','route')
+									->from('route')
 									->join('route_pi','route_pi.route_id','=','route.route_id')
 									->join('bus_route','bus_route.bus_id','=','bus.bus_id')
 									->join('route','route.route_id','=','bus_route.route_id')
