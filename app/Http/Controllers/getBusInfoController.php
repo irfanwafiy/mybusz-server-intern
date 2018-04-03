109dc990ee86c198d30648250ffea2f2258185e1
unknown <skyxsw7@gmail.com>
2018-04-03 15:11:55 +0800

    debugging

--- app/Http/Controllers/getBusInfoController.php
+++ app/Http/Controllers/getBusInfoController.php
@@ -358,7 +358,7 @@ class getBusInfoController extends Controller
 		//dd(json_encode($getETA_Query));
 		foreach($calcETA_Result as $result)
 		{
-			print($result->eta);
+			//print($result->eta);
 			$result->eta = self::processEta($currentTime, $result->eta);
 			
 			//print($result);
