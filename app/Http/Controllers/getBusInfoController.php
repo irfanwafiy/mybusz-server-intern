1d78c64abd22d9eeb4e454a3a890a50630bcf2c0
unknown <skyxsw7@gmail.com>
2018-07-16 14:32:21 +0800

    getBusRouteInfo changes

--- app/Http/Controllers/getBusInfoController.php
+++ app/Http/Controllers/getBusInfoController.php
@@ -75,7 +75,7 @@ class getBusInfoController extends Controller
 							->where('bus_service_no',$bus_no)
 							->get();
 		
-			print(json_encode($bus_route_info_route_id));
+			
 		foreach($bus_route_info_route_id as $r_id)
 		{
 			
@@ -88,8 +88,7 @@ class getBusInfoController extends Controller
 							->limit(1)
 							->get();
 			
-			print('Hello ');
-			print(json_encode($bus_route_info));
+			
 			
 			
 			foreach($bus_route_info as $singleset)
@@ -101,8 +100,7 @@ class getBusInfoController extends Controller
 				array_push($array_busRouteInfo, $singleset);
 				//$dataset_busRouteInfo->push($getBusRouteInfo_singleset);
 			}
-			print(' Array ');
-			print(json_encode($array_busRouteInfo));
+			
 			
 		}
 		
