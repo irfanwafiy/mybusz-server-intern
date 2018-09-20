38c07913bd32b3165a92b91b18cb171de0387854
unknown <skyxsw7@gmail.com>
2018-09-20 20:14:52 +0800

    add new Calls

--- app/Http/Controllers/getBusInfoController.php
+++ app/Http/Controllers/getBusInfoController.php
@@ -303,7 +303,7 @@ class getBusInfoController extends Controller
 											->where('route_id', $route)
 											->where('bus_stop_id', $bus_stop_id)
 											->first();
-			print_r("hello  ".$getBusStopID);
+			print_r($getBusStopID);
 			$route_order = $getBusStopID;
 		}
 
