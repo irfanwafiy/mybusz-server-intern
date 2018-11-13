f27de91e469de7a699db831e882bae345616f5f7
unknown <skyxsw7@gmail.com>
2018-11-13 21:16:01 +0800

    add bus_stop_info

--- resources/views/bus_stop_info.php
+++ resources/views/bus_stop_info.php
@@ -6,7 +6,7 @@
 <style>
 
 @font-face {
-    font-family: OptimusPrinceps;
+    font-family: data-latin_font;
     src: url('{{ public_path('fonts/data-latin_font.tff') }}');
 }
 
@@ -23,6 +23,11 @@ body {
    text-transform: uppercase;
 }
 table {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
     width: 100%;
     height: 100%;
 }
