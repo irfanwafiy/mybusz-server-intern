2c816b70b43357f4da340d1df19ae8133af45b9b
unknown <skyxsw7@gmail.com>
2018-11-14 00:05:07 +0800

    add bus_stop_info

--- resources/views/bus_stop_info.blade.php
+++ resources/views/bus_stop_info.blade.php
@@ -83,7 +83,7 @@ th {
            <th>Incoming</th>
            <th>Destination</th>
         </tr>
-        @foreach ($data as $value)
+        @foreach($data as $value)
         <tr>
            <td>{{$value->bus_service_no}}</td>
 
