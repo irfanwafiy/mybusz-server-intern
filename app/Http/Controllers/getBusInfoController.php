7be40d4051222106b5f2a0a2989c3295c12a1b51
unknown <skyxsw7@gmail.com>
2018-11-15 04:11:29 +0800

    add bus_stop_info

--- app/Http/Controllers/getBusInfoController.php
+++ app/Http/Controllers/getBusInfoController.php
@@ -666,6 +666,8 @@ class getBusInfoController extends Controller
 			array_push($getBusStopInfo_array, $dataset_busList);
 		}
 			$stop_name = $getBusStopName['name'];
+
+			dd($stop_name);
 		$data = [
 		'bus_data' => $getBusStopInfo_array,
 		'stop_name' => $stop_name
