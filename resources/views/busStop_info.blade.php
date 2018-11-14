3d09fe122adfca106e8cd6b319ea7453f27507ea
unknown <skyxsw7@gmail.com>
2018-11-15 06:05:07 +0800

    add bus_stop_info

--- resources/views/busStop_info.blade.php
+++ resources/views/busStop_info.blade.php
@@ -113,7 +113,7 @@ input[type=submit]:hover {
 				<div class="title m-b-md">
                     Bus Simulator
                 </div>
-				<form action="https://laravelsyd-fypfinalver.herokuapp.com/getBusStopInfo" method="get">
+				<form action="https://laravelsyd-fypfinalver.herokuapp.com/getBusStopInfo" method="post">
 
 				  <label for="bus_id">bus_stop_id:</label>
 				  <input id="bus_stop_id" type="text" name="bus_id" value="">
