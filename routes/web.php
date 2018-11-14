16eb332ce8de96d2b81bc96936e6a401c204c304
unknown <skyxsw7@gmail.com>
2018-11-15 04:37:41 +0800

    add bus_stop_info

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::post('/getmobile_nearbyStop', 'getBusInfoController@getmobile_nearbyStop'
 Route::post('/getbus_stops_eta', 'getBusInfoController@getbus_stops_eta');
 Route::post('/getbus_stop_bus_services', 'getBusInfoController@getbus_stop_bus_services');
 
-Route::post('/getBusStopInfo', 'getBusInfoController@getBusStopInfo');
+Route::get('/getBusStopInfo', 'getBusInfoController@getBusStopInfo');
 
 Route::get('/viewETATableGet', 'userController@viewETATableGet');
 
