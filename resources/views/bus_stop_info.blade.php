631047dcf57872365f79a7c9d6bd3e62656b5adb
unknown <skyxsw7@gmail.com>
2018-11-19 18:41:58 +0800

    add bus_stop_info

--- resources/views/bus_stop_info.blade.php
+++ resources/views/bus_stop_info.blade.php
@@ -151,7 +151,7 @@ function startTime() {
       document.getElementById("test").innerHTML = "" + eta_date;
       var eta_check = new Date(eta_date);
 
-      if(today >= eta_check)
+      if(today <= eta_check)
       {
 
         document.getElementById(index).innerHTML = "Update";
