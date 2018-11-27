20c73e7cf866199b8ef1bf646bbfe11a8d1141b7
unknown <skyxsw7@gmail.com>
2018-11-27 20:42:28 +0800

    add bus_stop_info

--- resources/views/bus_stop_info.blade.php
+++ resources/views/bus_stop_info.blade.php
@@ -221,10 +221,11 @@ function refresh_eta_post(bus_stop_id, refresh_array)
     var jsonResponse = JSON.parse(resp);
     for (var value in jsonResponse)
     {
-      console.log(value, jsonResponse[value])
+      console.log(value, jsonResponse[value]);
+      console.log("id :" + jsonResponse[value].key);
       var n = jsonResponse[value].key.indexOf("_");
       var get_id =  resp.substr(n,jsonResponse[value].key.length);
-      console.log("id :" + get_id);
+
       var index_eta = "eta" + get_id;
       var index_eta2 = "eta_b" + get_id;
       document.getElementById(index_eta).innerHTML = jsonResponse[value].stop_eta;
