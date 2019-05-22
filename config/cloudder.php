6c56eb14367f536fbf672ff8b4bf87357559d46f
unknown <skyxsw7@gmail.com>
2019-05-22 19:30:24 +0800

    add cloudinary

--- config/cloudder.php
+++ config/cloudder.php
@@ -19,9 +19,6 @@ return [
     // 'apiKey'     => env('CLOUDINARY_API_KEY'),
     // 'apiSecret'  => env('CLOUDINARY_API_SECRET'),
     'cloudName'  => 'hsj2bliee',
-    'baseUrl'    => env('CLOUDINARY_BASE_URL', 'http://res.cloudinary.com/'.env('CLOUDINARY_CLOUD_NAME')),
-    'secureUrl'  => env('CLOUDINARY_SECURE_URL', 'https://res.cloudinary.com/'.env('CLOUDINARY_CLOUD_NAME')),
-    'apiBaseUrl' => env('CLOUDINARY_API_BASE_URL', 'https://api.cloudinary.com/v1_1/'.env('CLOUDINARY_CLOUD_NAME')),
     'apiKey'     => '942956192113262',
     'apiSecret'  => 'ro_SlWCH5XLrbsrh0KhTq0a6Q4Q',
 
