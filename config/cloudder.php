61a2dc53826827962bfc6e39567092c164b312df
unknown <skyxsw7@gmail.com>
2019-05-22 18:47:30 +0800

    add cloudinary

--- config/cloudder.php
+++ config/cloudder.php
@@ -12,12 +12,18 @@ return [
     |
     */
 
-    'cloudName'  => env('CLOUDINARY_CLOUD_NAME'),
+    // 'cloudName'  => env('CLOUDINARY_CLOUD_NAME'),
+    // 'baseUrl'    => env('CLOUDINARY_BASE_URL', 'http://res.cloudinary.com/'.env('CLOUDINARY_CLOUD_NAME')),
+    // 'secureUrl'  => env('CLOUDINARY_SECURE_URL', 'https://res.cloudinary.com/'.env('CLOUDINARY_CLOUD_NAME')),
+    // 'apiBaseUrl' => env('CLOUDINARY_API_BASE_URL', 'https://api.cloudinary.com/v1_1/'.env('CLOUDINARY_CLOUD_NAME')),
+    // 'apiKey'     => env('CLOUDINARY_API_KEY'),
+    // 'apiSecret'  => env('CLOUDINARY_API_SECRET'),
+    'cloudName'  => 'hsj2bliee',
     'baseUrl'    => env('CLOUDINARY_BASE_URL', 'http://res.cloudinary.com/'.env('CLOUDINARY_CLOUD_NAME')),
     'secureUrl'  => env('CLOUDINARY_SECURE_URL', 'https://res.cloudinary.com/'.env('CLOUDINARY_CLOUD_NAME')),
     'apiBaseUrl' => env('CLOUDINARY_API_BASE_URL', 'https://api.cloudinary.com/v1_1/'.env('CLOUDINARY_CLOUD_NAME')),
-    'apiKey'     => env('CLOUDINARY_API_KEY'),
-    'apiSecret'  => env('CLOUDINARY_API_SECRET'),
+    'apiKey'     => '942956192113262',
+    'apiSecret'  => 'ro_SlWCH5XLrbsrh0KhTq0a6Q4Q',
 
     'scaling'    => [
         'format' => 'png',
