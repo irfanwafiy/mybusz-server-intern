aabc0f5e9963dfcfc556d1a239e28a132191223a
unknown <skyxsw7@gmail.com>
2019-05-22 19:31:18 +0800

    add cloudinary

--- app/Http/Controllers/CRUDController.php
+++ app/Http/Controllers/CRUDController.php
@@ -23,7 +23,7 @@ class CRUDController extends Controller
 
     $file = $request->file('file')->getRealPath();
 
-     Cloudder::unsigned_upload($file, null,array("resource_type" => "auto"));
+     Cloudder::upload($file,array("resource_type" => "auto"));
 
      return redirect()->back()->with('status', 'File Uploaded Successfully');
 
