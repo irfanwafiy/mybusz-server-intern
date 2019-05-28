ef739802db7ad83d9b6a43b61c207bdc9930ba44
unknown <skyxsw7@gmail.com>
2019-05-28 15:39:31 +0800

    testing files

--- app/Http/Controllers/CRUDController.php
+++ app/Http/Controllers/CRUDController.php
@@ -28,7 +28,7 @@ class CRUDController extends Controller
 
      //return redirect()->back()->with('status', 'File Uploaded Successfully');
      $filecontent = file_get_contents($url);
-     $result = explode(PHP_EOL,$filecontent);
+     $result = explode(','.PHP_EOL,$filecontent);
 
      print_r($result);
   }
